81b8d6038d6b443e593e971045021af965571954
Carlos Romero <c6099020@gmail.com>
2024-01-21 11:02:33 -0500

    Exercises 13&14, refactoring with express-async-errors library, and using validators

--- bloglist/controllers/blogs.js
+++ bloglist/controllers/blogs.js
@@ -6,25 +6,27 @@ blogsRouter.get("/", async (request, response) => {
   response.json(blogs)
 })
 
-blogsRouter.post("/", async (request, response, next) => {
-
-  if (!request.body.likes){
-    request.body.likes = 0;
-  }
-
-
-  if (!request.body.title || !request.body.url){
-    console.log("error", request.body)
-    return response.status(400).end()
-  }
+blogsRouter.post("/", async (request, response) => {
 
   const blog = new Blog(request.body)
 
-  await blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+  const savedblog = await blog.save()
+  response.status(201).json(savedblog)
+})
+
+blogsRouter.delete("/:id", async (request, response) => {
+
+  await Blog.findByIdAndDelete(request.params.id);
+
+  response.status(204).end();
+})
+
+blogsRouter.put("/:id", async (request, response) => {
+
+
+  const updatedNote = await Blog.findByIdAndUpdate(request.params.id, request.body, { new: true, runValidators: true, context: "query" })
+
+  response.json(updatedNote)
 })
 
 module.exports = blogsRouter
